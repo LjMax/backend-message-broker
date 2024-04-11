4599a05c969d9d343403afd9bde8094453a2547c
Ljubomir Maksimovic <ljubomir.maksimovic@vivaconsulting.rs>
2024-04-11 20:25:49 +0200

    Added optional command line parameters

--- main.cpp
+++ main.cpp
@@ -2,12 +2,40 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <string>
 
-int main() {
+int main(int argc, char *argv[]) {
     // Replace these
     std::string brokerUrl = "amqp://localhost:5672";
-    std::string address = "testQueue";
-    std::string dbPath = "messages.db";
+    std::string address   = "testQueue";
+    std::string dbPath    = "messages.db";
+    std::string message   = "";
+
+    // accept command line arguments (optional)
+    if(argc > 1) {
+        switch (argc) {
+        case 2:
+            brokerUrl = argv[1];
+            break;
+        case 3:
+            brokerUrl = argv[1];
+            address   = argv[2];
+            break;
+        case 4:
+            brokerUrl = argv[1];
+            address   = argv[2];
+            dbPath    = argv[3];
+            break;
+        case 5:
+            brokerUrl = argv[1];
+            address   = argv[2];
+            dbPath    = argv[3];
+            message   = argv[4];
+            break;
+        default:
+            break;
+        }
+    }
 
     MessageHandler handler(brokerUrl, address, dbPath);
 
@@ -15,7 +43,7 @@ int main() {
     handler.waitForSender();
 
     // Send a message
-    handler.sendMessage("Test message!");
+    handler.sendMessage(message.empty() ? "Test message!" : message);
 
     // Wait for the message to be received and processed
     std::this_thread::sleep_for(std::chrono::seconds(10));
